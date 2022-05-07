d74f6b3e680a2dacc4e0115906a6c44381f51ede
tulipIC337 <101989381+tulipIC337@users.noreply.github.com>
2022-05-07 19:36:20 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spammer-SMS
+Spammer-SMS By.Lil slip up
+เอาไปขายก็เรื่องของมึง
+api : https://portal.clickatell.com/#/
---
+++ spammer-sms by.LIl เพลี่ยงพล้ำ/spammer-sms by.LIl เพลี่ยงพล้ำ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace spammer_sms_by.LIl_เพลี่ยงพล้ำ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient web = new WebClient();
+            Console.Title = "Spammer-SMS By.Lil เหลี่ยงหล้ำ";
+            Console.Write("Phone = ");
+            string phone = Console.ReadLine();
+            Console.Write("Message = ");
+            string msg = Console.ReadLine();
+            int i = 0;
+            while(i <= 99999999999)
+            {
+                web.DownloadString($"https://platform.clickatell.com/messages/http/send?apiKey=bddu4b_4RC6yKrGrA8AfKw==&to={phone}&content={msg}" + DateTime.Now);
+            }
+            Console.Read();
+        }
+    }
+}
